4fd51129f0197131893a920049b9ae1059228a34
American Computers <American Computers@LIGHTONRAY>
2022-01-05 21:28:02 +0100

    Add project files.

---
+++ ValidEmailAddressFinderApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Net.Mail;
+using System.Text.RegularExpressions;
+
+namespace EmailValidationApp
+
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Please enter your E-mail address: ");
+			string email = Console.ReadLine();
+
+			if (isValidEmail(email))
+			{
+				Console.WriteLine("Yes, that's a valid e-mail address");
+			}
+			else
+			{
+				Console.WriteLine("No, that's not a valid e-mail address");
+			}
+		}
+
+		private static bool isValidEmail(string email)
+		{
+			try
+			{
+				MailAddress mail = new MailAddress(email);
+				return true;
+			}
+			catch
+			{
+				return false;
+			}
+		}
+	}
+
+}
